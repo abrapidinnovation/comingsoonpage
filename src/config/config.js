4f9253b5cf6571c444c3b4c144e6ea46dba819ec
abrapidinnovation <ab@rapidinnovation.dev>
2020-02-04 11:49:13 +0530

    first-commit

---
+++ src/config/config.js
@@ -0,0 +1,43 @@
+import slide1 from "../assets/slide1.jpg";
+import slide2 from "../assets/slide2.jpg";
+import slide3 from "../assets/slide3.jpg";
+import slide4 from "../assets/slide4.jpg";
+import slide5 from "../assets/slide5.jpg";
+import slide6 from "../assets/slide6.jpg";
+import slide7 from "../assets/slide7.jpg";
+import slide8 from "../assets/slide8.jpg";
+import slide9 from "../assets/slide9.jpg";
+import slide10 from "../assets/slide10.jpg";
+import logo from "../assets/logo.png";
+
+export const slides = [
+  slide1,
+  slide2,
+  slide3,
+  slide4,
+  slide5,
+  slide6,
+  slide7,
+  slide8,
+  slide9,
+  slide10
+];
+
+export const info = {
+  Heading: "COMING SOON",
+  Description: "Our website is under construction",
+  Headingcolor: "rgb(119,195,97)"
+};
+
+export const timer = {
+  endtimerat: "02 14 2020, 6:00 am",
+  timerformat: "MM DD YYYY, h:mm a"
+};
+
+export const Logo = logo;
+export const logostatus = true;
+
+export const colors = {
+  Arccolor: "rgb(119,195,97)",
+
+};
